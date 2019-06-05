fd81b955bafaa4b0ad84a19009c74c18b3244202
gonzalinismo <30642624+gonzalinismo@users.noreply.github.com>
2019-06-05 11:52:52 -0300

    Actualizacion de estilo

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
 
-if (typeof jQuery == 'undefined') {  
-    console.log("jQuery is not loaded");  
-    //or
-    alert("jQuery is loaded");  
-} else {
-    console.log("jQuery is not loaded");
-     alert("jQuery is not loaded"); 
-}
-
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
